17dc0be015e1d0cfbb76147bab75baf9bf290e27
ErikCoelho <erikcoelhomoraes84@gmail.com>
2023-01-08 19:49:42 -0300

    update program

--- Shop.Domain.Api/Program.cs
+++ Shop.Domain.Api/Program.cs
@@ -33,11 +33,8 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("DevPolicy");
 app.UseHttpsRedirection();
